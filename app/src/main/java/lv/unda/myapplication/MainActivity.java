df5dd8484e414d4dc18a9f6d6fc0695897acdf78
undazh <undazurevska@gmail.com>
2023-10-23 12:32:38 +0300

    Json saving added

--- app/src/main/java/lv/unda/myapplication/MainActivity.java
+++ app/src/main/java/lv/unda/myapplication/MainActivity.java
@@ -1,31 +1,38 @@
 package lv.unda.myapplication;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import android.Manifest;
-import android.content.ContentResolver;
-import android.content.pm.PackageManager;
 
-import android.database.Cursor;
-import android.net.Uri;
-import android.widget.Toast;
-import android.Manifest;
-import android.app.AlertDialog;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.io.FileInputStream;
 
-import android.os.Bundle;
-import android.provider.ContactsContract;
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.view.View;
-
-
 public class MainActivity extends AppCompatActivity {
-    private ArrayList<String> selectedContacts = new ArrayList<>();
+    private ArrayList<Contact> selectedContacts = new ArrayList<>();
     private ListView contactListView;
-    private ArrayAdapter<String> adapter;
+    private ArrayAdapter<Contact> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,14 +42,20 @@ public class MainActivity extends AppCompatActivity {
         contactListView = findViewById(R.id.contactListView);
         adapter = new ArrayAdapter<>(
                 this,
-                R.layout.contact_list_item, // Layout for each item
-                R.id.contactName,            // ID of the TextView for the name
+                R.layout.contact_list_item,
+                R.id.contactName,
                 selectedContacts
         );
         contactListView.setAdapter(adapter);
+
+        ArrayList<Contact> loadedContacts = loadContactsFromJson();
+        if (loadedContacts != null) {
+            selectedContacts.addAll(loadedContacts);
+            adapter.notifyDataSetChanged();
+        }
     }
 
-    public void btnGetContactPressed (View v) {
+    public void btnGetContactPressed(View v) {
         getPhoneContacts();
     }
 
@@ -59,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
                 builder.setTitle("Select Contacts");
 
                 final ArrayList<String> contactNames = new ArrayList<>();
-                final ArrayList<String> contactNumbers = new ArrayList();
+                final ArrayList<String> contactNumbers = new ArrayList<>();
                 final boolean[] checkedContacts = new boolean[cursor.getCount()];
 
                 while (cursor.moveToNext()) {
@@ -80,14 +93,19 @@ public class MainActivity extends AppCompatActivity {
                         checkedContacts,
                         (dialog, which, isChecked) -> {
                             if (isChecked) {
-                                selectedContacts.add(contactNames.get(which) + ": " + contactNumbers.get(which));
+                                String name = contactNames.get(which);
+                                String phoneNumber = contactNumbers.get(which);
+                                Contact contact = new Contact(name, phoneNumber);
+                                selectedContacts.add(contact);
                             } else {
-                                selectedContacts.remove(contactNames.get(which) + ": " + contactNumbers.get(which));
+                                selectedContacts.remove(selectedContacts.get(which));
                             }
+                            updateListAdapter();
                         }
                 );
 
                 builder.setPositiveButton("OK", (dialog, which) -> {
+                    saveContactsToJson(selectedContacts);
                     adapter.notifyDataSetChanged();
                 });
 
@@ -97,10 +115,59 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 Toast.makeText(this, "No contacts found on the phone.", Toast.LENGTH_SHORT).show();
             }
-
-
         }
     }
 
+    private void saveContactsToJson(ArrayList<Contact> contacts) {
+        Gson gson = new Gson();
+        String json = gson.toJson(contacts);
 
+        try {
+            FileOutputStream outputStream = openFileOutput("selected_contacts.json", Context.MODE_PRIVATE);
+            outputStream.write(json.getBytes());
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private ArrayList<Contact> loadContactsFromJson() {
+        ArrayList<Contact> loadedContacts = new ArrayList<>();
+        try {
+            FileInputStream inputStream = openFileInput("selected_contacts.json");
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+            inputStream.close();
+
+            String json = stringBuilder.toString();
+
+            Gson gson = new Gson();
+            Type contactListType = new TypeToken<ArrayList<Contact>>() {
+            }.getType();
+            loadedContacts = gson.fromJson(json, contactListType);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return loadedContacts;
+    }
+
+    private void updateListAdapter() {
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults); // Call the superclass implementation
+
+        if (requestCode == 0 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            getPhoneContacts();
+        } else {
+            Toast.makeText(this, "Permission denied. Cannot access contacts.", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
